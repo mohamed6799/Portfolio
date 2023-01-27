9097a186bec4ef26e40807db3d4626b16baf0758
m7md6799 <m7md@gmail.com>
2023-01-27 20:55:02 +0200

    first day in project

---
+++ script/main.js
@@ -0,0 +1,8 @@
+
+let settingsBut = document.querySelectorAll(".content .settings .buttons button");
+
+let borderImg = document.querySelectorAll(".nav img")[0];
+// settingsBut[0].onclick=function(){
+//     borderImg.style.border="5px solid var(--OneColor)"
+//     colorh1.style.color="var(--OneColor)"
+// }
