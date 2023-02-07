3a40e1f40b084f596eef3a0f5c6bd626380e146e
m7md6799 <m7md@gmail.com>
2023-02-07 20:09:06 +0200

    finish portfolio project

--- script/main.js
+++ script/main.js
@@ -1,20 +1,28 @@
-let settingsBut = document.querySelectorAll(".content .settings .buttons button");
-let navItem = document.querySelectorAll(".menu ul li a");
 let navbar = document.querySelectorAll(".nav .navbar")[0];
-let borderImg = document.querySelectorAll(".nav  img")[0];
 let h1 = document.querySelectorAll(".nav  h1")[0];
 let menu = document.querySelectorAll(".nav .menu")[0];
+let settingsBut = document.querySelectorAll(".content .settings .buttons button");
 let content = document.querySelectorAll(".portfolio .content")[0];
 let item = document.querySelectorAll(".portfolio .content .item");
-let borderAnimation = document.querySelectorAll(".portfolio .content .item .projects .bor");
-let buttonIncard = document.querySelectorAll(".but");
 let darkmode = document.getElementById("checkdark");
 let buttondark = document.querySelectorAll(".input2 label")[0];
-let spanNameH1 = document.querySelectorAll(".content .home .title h1 span");
-let buttonHome = document.querySelectorAll(".content .home .title button")[0];
-let contentImgborder = document.querySelectorAll(".content .home .img div");
-let textHover = document.querySelectorAll(".portfolio .content .courses .units .unit .text_hover");
 let coursesUnit = document.querySelectorAll(".portfolio .content .courses .units .unit");
+
+// border = bdr
+let borderabout = document.querySelectorAll(".about .units .unit .twounit .minunit");
+let contentImgborder = document.querySelectorAll(".content .home .img div");
+let borderImg = document.querySelectorAll(".nav  img")[0];
+
+//color = oor
+let navItem = document.querySelectorAll(".menu ul li a");
+let spanNameH1 = document.querySelectorAll(".content .home .title h1 span");
+
+// bg
+let downloadcv = document.getElementById("cv");
+let textHover = document.querySelectorAll(".portfolio .content .courses .units .unit .text_hover");
+let borderAnimation = document.querySelectorAll(".portfolio .content .item .projects .bor");
+let buttonIncard = document.querySelectorAll(".but");
+let buttonHome = document.querySelectorAll(".content .home .title button")[0];
 
 opacityImg()
 function opacityImg(){
@@ -123,46 +131,45 @@ function activeColor(e){
                 `;
             }
         })
-        contentImgborder.forEach(m=> m.style.cssText = `
-        border-color:${e.style.backgroundColor};
-        transition:0.3s ease;
-        
-        `)
+        contentImgborder.forEach(m=> {
+            m.style.cssText = `
+                border-color:${e.style.backgroundColor};
+                transition:0.3s ease;
+            `;
+        })
         spanNameH1.forEach(m=> {
             m.style.cssText = `
             color:${e.style.backgroundColor};
             transition:0.3s ease;
             `})
-            buttonHome.style.cssText = `
-            background-color:${e.style.backgroundColor};
-            
-            `;
-            buttonIncard.forEach(m=>{
+        buttonIncard.forEach(m=>{
                 m.style.cssText = `
                 background-color:${e.style.backgroundColor}
                 `;    
-            })
-            borderAnimation.forEach(m=>{
+        })
+        borderAnimation.forEach(m=>{
                 m.style.cssText = `
                 background-color:${e.style.backgroundColor}
                 `
-            })
-            
-            textHover.forEach(m=>{
+        })
+        textHover.forEach(m=>{
                 m.style.cssText = `
                 background-color:${e.style.backgroundColor}
                 `
-            })
-            // coursesUnit.forEach(m=>{
-            //     m.style.cssText = `
-            //     border:5px solid ${e.style.backgroundColor};
-            //     `
-            // })
-        
-        
+        })
+        borderabout.forEach(m=>{
+                m.style.cssText = `
+                transition:0.3s ease;
+                border:2px solid ${e.style.backgroundColor};
+                `
+        })
+        downloadcv.style.cssText = `
+            background-color:${e.style.backgroundColor}
+        `;
+        buttonHome.style.cssText = `
+            background-color:${e.style.backgroundColor};
             
-            
-
+        `;    
     }
 }
 
